e896dd40aa45e645104fb6b85c74ce7822191f0c
mateusz <spaaw@poczta.fm>
2021-09-13 22:02:42 +0200

    Use stoppingDistance from navMeshAgent to detect collision between dale and enemy.

--- Assets/EnemyCollisionController.cs
+++ Assets/EnemyCollisionController.cs
@@ -1,23 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyCollisionController : MonoBehaviour
 {
     private GameObject player;
     public float stoppingDistance = 1.2f;
     private GameManagement gameManagement;
+    private NavMeshAgent navMeshAgent;
 
     void Start()
     {
         player = GetPlayer.instance.player;
         gameManagement = FindObjectOfType<GameManagement>();
+        navMeshAgent = FindObjectOfType<NavMeshAgent>();
 
     }
 
     void Update()
     {
-        if (Vector3.Distance(transform.position, player.transform.position) < stoppingDistance)
+        if (Vector3.Distance(transform.position, player.transform.position) < navMeshAgent.stoppingDistance)
         {
             gameManagement.DecreasePlayerHP();
 
