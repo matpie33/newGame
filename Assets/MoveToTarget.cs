4b8b843774b1e5f29f46f9d9bba1b7ccd7e61d23
mateusz <spaaw@poczta.fm>
2021-11-01 18:52:26 +0100

    Make enemy properly follow player.

---
+++ Assets/MoveToTarget.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToTarget : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+
+    [SerializeField]
+    private Transform playerPosition;
+
+    [SerializeField]
+    private float minimumDistanceToFollowPlayer = 5;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(playerPosition.position, gameObject.transform.position) < minimumDistanceToFollowPlayer)
+        {
+            navMeshAgent.destination = playerPosition.transform.position;
+        }
+
+
+
+    }
+
+
+}
