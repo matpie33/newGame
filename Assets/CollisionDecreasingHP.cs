89d35caa39214883fb81df8b2983882e27006493
mateusz <spaaw@poczta.fm>
2021-11-28 19:25:24 +0100

    Add script that decreases dale hp when he's near a cactus.

---
+++ Assets/CollisionDecreasingHP.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDecreasingHP : MonoBehaviour
+{
+    private GameObject player;
+    [SerializeField]
+    private float distanceOnWhichDecreaseHp = 0.8f;
+
+    private GameManagement gameManagement;
+
+    public void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        gameManagement = GameManagement.instance;
+    }
+
+    public void Update()
+    {
+        if (Vector3.Distance(player.transform.position, gameObject.transform.position) < distanceOnWhichDecreaseHp)
+        {
+            gameManagement.DecreasePlayerHP();
+        }
+    }
+
+
+}
